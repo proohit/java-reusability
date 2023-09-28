98a6a2d0985c079f967a607b945d6a06fec870ff
proohit <direnc99@gmail.com>
2023-09-28 16:11:35 +0000

    feat: implement external fn registration WIP

--- shared-server-rs/src/main.rs
+++ shared-server-rs/src/main.rs
@@ -1,9 +1,7 @@
 use jni::{
-    descriptors::Desc,
-    errors::{Error, Result},
-    objects::{AutoLocal, JValue, JString, JObject},
+    errors::Result,
+    objects::{JObject, JValue},
     signature::{Primitive, ReturnType},
-    sys::{jint, JavaVMInitArgs},
     InitArgsBuilder, JNIEnv,
 };
 use std::sync::{Arc, Once};
@@ -32,7 +30,8 @@ pub fn jvm() -> &'static Arc<JavaVM> {
         let jvm_args = InitArgsBuilder::new()
             .version(JNIVersion::V8)
             .option("-Xcheck:jni")
-            .option("-Djava.class.path=/home/master/workspace/java-reusability/shared-server/lib/bin/main")
+            .option("-Djava.class.path=/home/master/workspace/java-reusability/shared-server/lib/build/classes/java/main")
+            .option("-Djava.library.path=/home/master/workspace/java-reusability/shared-server-rs/target/debug")
             .build()
             .unwrap_or_else(|e| panic!("{:#?}", e));
 
@@ -51,69 +50,51 @@ pub fn attach_current_thread() -> AttachGuard<'static> {
         .attach_current_thread()
         .expect("failed to attach jvm thread")
 }
-static EXCEPTION_CLASS: &str = "java/lang/Exception";
-static TESTING_OBJECT_STR: &str = "TESTING OBJECT";
 
 fn main() {
     let mut env = attach_current_thread();
 
     let server_class = env.find_class("shared/server/Server").unwrap();
 
-    // let add_route_method_id = env
-    //     .get_static_method_id(&server_class, "addRoute", "(Lshared/server/RouteHandler;)V")
-    //     .unwrap();
+    let add_route_method_id = env
+        .get_static_method_id(&server_class, "addRoute", "(Ljava/lang/String;)I")
+        .unwrap();
+    let string_path = env.new_string("/test").unwrap();
+    let string_class = env.find_class("java/lang/String").unwrap();
+    let ctor_method_id = env
+        .get_method_id(&string_class, "<init>", "(Ljava/lang/String;)V")
+        .unwrap();
+    let j_string_path: JObject = unsafe {
+        env.new_object_unchecked(
+            &string_class,
+            ctor_method_id,
+            &[JValue::from(&string_path).as_jni()],
+        )
+    }
+    .unwrap();
 
-    // let test_str = env.new_string("/").unwrap();
-
-    // // Get a reference to the Function interface class
-    // let function_class = env
-    //     .find_class("java/util/function/Function")
-    //     .expect("Failed to find Function class");
-
-    // // Find the apply method of the Function interface
-    // let apply_method_id = env
-    //     .get_method_id(
-    //         function_class,
-    //         "apply",
-    //         "(Ljava/lang/Object;)Ljava/lang/Object;",
-    //     )
-    //     .expect("Failed to get apply method");
-
-    // // Create a Rust closure that implements the Function<String, String> interface
-    // let function = Box::new(|env: &JNIEnv, input: JString| -> JString {
-    //     let input_str = env.get_string(&input).expect("Couldn't convert input to String");
-    //     let input_str = input_str.to_str().unwrap();
-    //     let result_str = input_str.to_uppercase();
-    //     let result_jstring = env.new_string(result_str).expect("Couldn't create JString");
-    //     result_jstring.into()
-    // });
-
-    // // Convert the closure to a jobject representing a Function<String, String>
-    // let function_obj = JValue::Object(function);
+    let handler_id = unsafe {
+        env.call_static_method_unchecked(
+            &server_class,
+            add_route_method_id,
+            ReturnType::Primitive(Primitive::Int),
+            &[JValue::Object(&j_string_path).as_jni()],
+        )
+    }
+    .unwrap()
+    .i()
+    .unwrap();
+    println!("registered handler {}", handler_id);
     
-    // // Wrap the function_obj in a JValue
-    // let function_arg = JValue::from(function_obj);
-
-    // let route_handler_class = env.find_class("shared/server/RouteHandler").unwrap();
-    
-    // let route_handler_1 = env.new_object(&route_handler_class, "(Ljava/lang/String;Ljava/util/function/Function<Ljava/lang/String;Ljava/lang/String;>)", &[JValue::from(&test_str)]).unwrap();
-    // unsafe {
-    //     let _ = env.call_static_method_unchecked(
-    //         &server_class,
-    //         add_route_method_id,
-    //         ReturnType::Primitive(Primitive::Void),
-    //         &[JValue::Object(&route_handler_1).as_jni()],
-    //     );
-    // }
     let start_method_id = env
-        .get_static_method_id(&server_class, "start", "()V")
+        .get_static_method_id(&server_class, "start", "(Z)V")
         .unwrap();
     let _ = unsafe {
         env.call_static_method_unchecked(
             &server_class,
             start_method_id,
             ReturnType::Primitive(Primitive::Void),
-            &[],
+            &[JValue::Bool(1).as_jni()],
         )
     }
     .unwrap()
--- shared-server/lib/src/main/java/shared/server/Server.java
+++ shared-server/lib/src/main/java/shared/server/Server.java
@@ -5,6 +5,8 @@ import java.net.InetSocketAddress;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 import com.sun.net.httpserver.HttpExchange;
@@ -14,33 +16,66 @@ public class Server {
 
     private static final int PORT = 8001;
     private static Map<Integer, RouteHandler> routeHandlers = new HashMap<>();
+    private static Map<String, Integer> routeHandlersExt = new HashMap<>();
+
+    private static native String handle_request_external(int id, String req);
 
     public static void addRoute(RouteHandler routeHandler) {
         int routeIndex = Server.routeHandlers.size();
         Server.routeHandlers.put(routeIndex, routeHandler);
     }
 
-    public static void start() throws IOException {
+    public static int addRoute(String path) {
+        int routeIndex = Server.routeHandlersExt.size();
+        Server.routeHandlersExt.put(path, routeIndex);
+        System.out.println(String.format("[JAVA] registered handler path %s", path));
+        return routeIndex;
+    }
+
+    public static void start(boolean lib) throws IOException {
+
+        if (lib) {
+            System.loadLibrary("shared_server_rs");
+        }
 
         HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", PORT), 0);
 
-        List<RouteHandler> routeHandlersList = Server.routeHandlers.values().stream()
-                .collect(Collectors.toList());
-        for (RouteHandler routeHandler : routeHandlersList) {
-            httpServer.createContext(routeHandler.getPath(), exchange -> {
-                logIncomingRequest(exchange);
+        if (lib) {
+            Set<Entry<String, Integer>> routeHandlersExtList = Server.routeHandlersExt.entrySet();
+            for (Entry<String, Integer> routeHandler : routeHandlersExtList) {
+                System.out.println(String.format("[JAVA] registering context for %s", routeHandler.getKey()));
+                httpServer.createContext(routeHandler.getKey(), exchange -> {
+                    logIncomingRequest(exchange);
+                    boolean matches = exchange.getRequestURI().getPath().equals(routeHandler.getKey());
+                    System.out.println(matches);
+                    if (matches) {
+                        String request = new String(exchange.getRequestBody().readAllBytes());
+                        String response = handle_request_external(routeHandler.getValue(), request);
+                        exchange.sendResponseHeaders(200, response.getBytes().length);
+                        exchange.getResponseBody().write(response.getBytes());
+                        exchange.close();
+                    }
+                });
+            }
+        } else {
+            List<RouteHandler> routeHandlersList = Server.routeHandlers.values().stream()
+                    .collect(Collectors.toList());
+            for (RouteHandler routeHandler : routeHandlersList) {
+                httpServer.createContext(routeHandler.getPath(), exchange -> {
+                    logIncomingRequest(exchange);
 
-                if (send404IfNoMatchingPath(exchange, routeHandler)) {
-                    return;
-                }
+                    if (send404IfNoMatchingPath(exchange, routeHandler)) {
+                        return;
+                    }
 
-                sendResponseWithHandler(routeHandler, exchange);
-            });
+                    sendResponseWithHandler(routeHandler, exchange);
+                });
+            }
         }
 
         httpServer.start();
 
-        System.out.println(String.format("Server started on port %d", PORT));
+        System.out.println(String.format("[JAVA] Server started on port %d in library mode %s", PORT, lib));
 
     }
 
@@ -57,8 +92,19 @@ public class Server {
                 exchange.getHttpContext().getPath()));
     }
 
-    private static boolean send404IfNoMatchingPath(HttpExchange exchange, RouteHandler routeHandler) throws IOException {
+    private static boolean send404IfNoMatchingPath(HttpExchange exchange, RouteHandler routeHandler)
+            throws IOException {
         if (!exchange.getRequestURI().getPath().equals(routeHandler.getPath())) {
+            exchange.sendResponseHeaders(404, 0);
+            exchange.close();
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean send404IfNoMatchingPath(HttpExchange exchange, boolean matches)
+            throws IOException {
+        if (!matches) {
             exchange.sendResponseHeaders(404, 0);
             exchange.close();
             return true;
